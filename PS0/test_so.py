1067806519559f6e4e2b83b2992219f6979321f6
Max Zuo <max.zuo@gmail.com>
2020-08-20 15:20:55 -0400

    deleted extra file

--- PS0/test_so.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import timeit
-
-list_time = timeit.timeit(stmt="a = [1, 2, 3, 4, 5]", number=1000000)
-ndarray_time = timeit.timeit(stmt=lambda: "a = np.array([1, 2, 3, 4, 5])", setup="import numpy as np", number=1000000)
